8da780edad67f5e00e6f1971f29f00d0511c1d7c
floatec <mp@floatec.de>
2013-07-30 19:36:45 +0200

    menuscreen

---
+++ DieHoelleIstVoll/Screens/MenuScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DieHoelleIstVoll
+{
+  class MenuScreen : Screen
+    {
+        
+
+        SpriteFont font;
+
+   
+
+        public MenuScreen()
+           
+        {
+           
+        }
+
+       
+
+        public override void Update(float dt)
+        {
+           
+        }
+
+      
+
+        public override void Draw()
+        {
+           
+        }
+    }
+
+}
